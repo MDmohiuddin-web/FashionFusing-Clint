444639d49b075b484d7289a03b546fcdba7313d0
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-09-13 19:03:58 +0600

    trying to make new thinks

---
+++ src/Routs/Routs.jsx
@@ -0,0 +1,36 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Product from "../pages/Product";
+import Cart from "../pages/Cart";
+import Collection from "../pages/Collection";
+import PlaceOrder from "../pages/PlaceOrder";
+import Order from "../pages/Order";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Contact from "../pages/Contact";
+import About from "../pages/About";
+import Navbar from "../components/Navbar";
+
+const Routs = () => {
+  return (
+    <div className="px-4 sm:px-[7vw] lg:px-[9vw] ">
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path="'/" element={<Home></Home>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="/order" element={<Order></Order>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/product/:productId" element={<Product></Product>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        {/* <Route path="/profile" element={<Profile></Profile>}></Route> */}
+        <Route path="/collection" element={<Collection></Collection>}></Route>
+        <Route path="/placeorder" element={<PlaceOrder></PlaceOrder>}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default Routs;
