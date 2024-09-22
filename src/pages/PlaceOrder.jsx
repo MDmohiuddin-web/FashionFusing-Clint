7908072a60c4e3dd9f075fcab36f60f984a21266
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-09-22 22:38:43 +0600

    error fix

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -5,13 +5,81 @@ const PlaceOrder = () => {
     <div className="flex flex-col sm:flex-row gap-4 justify-between sm:pt-14 min-h-[80vh] border-t">
       {/* left side */}
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px] ">
-<div className="text-xl sm:text-2xl my-3">
-    <Title text1={'DELIVERY'}   text2={'INFORMATION'}></Title>
-</div>
-<div className="flex gap-3">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"}></Title>
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="text"
+            name=""
+            className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+            placeholder=" First Name  "
+            id=""
+          />
+          <input
+            type="text"
+            name=""
+            className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+            placeholder="Last Name  "
+            id=""
+          />
+        </div>
+        <input
+          type="email"
+          name=""
+          className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+          placeholder="Email  "
+          id=""
+        />
+        <input
+          type="email"
+          name=""
+          className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+          placeholder="Street "
+          id=""
+        />
+        <div className="flex gap-3">
+          <input
+            type="text"
+            name=""
+            className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+            placeholder=" City  "
+            id=""
+          />
+          <input
+            type="text"
+            name=""
+            className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+            placeholder="State  "
+            id=""
+          />
+        </div>
 
-</div>
+        <div className="flex gap-3">
+          <input
+            type="number"
+            name=""
+            className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+            placeholder=" Zip Code  "
+            id=""
+          />
+          <input
+            type="text"
+            name=""
+            className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+            placeholder="Country  "
+            id=""
+          />
+        </div>
 
+        <input
+          type="tel"
+          name=""
+          className="px-3.5 py-1.5 w-full rounded border-gray-300 border"
+          placeholder="Phone "
+          id=""
+        />
+        {/* <input type="submit" value="Continue" className="bg-black text-white px-3.5 py-1.5 w-full rounded border-gray-300 border" /> */}
       </div>
     </div>
   );
