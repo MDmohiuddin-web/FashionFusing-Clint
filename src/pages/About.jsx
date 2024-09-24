883f24d9df1cc9322a67220d14ed1afb80fc73bf
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-09-24 20:20:40 +0600

    fixing issue

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,51 @@
-
+import { assets } from "../assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
+import Title from "../components/Title";
 
 const About = () => {
-    return (
-        <div>
-            <h4>this is about page</h4>
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"}></Title>
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          src={assets.about_img}
+          className="w-full md:max-w-[450px] "
+          alt=""
+        />
+        <div className="flex flex-col gap-6 justify-center md:w-2/4 text-gray-600">
+          <p>
+            Forever was born out of a passion for innovation and a desire to
+            revolutionize the way people shop online. Our journey began with a
+            simple idea: to provide a platform where customers can easily
+            discover, explore, and purchase a wide range of products from the
+            comfort of their homes.
+          </p>
+
+          <p>
+            Since our inception, we've worked tirelessly to curate a diverse
+            selection of high-quality products that cater to every taste and
+            preference. From fashion and beauty to electronics and home
+            essentials, we offer an extensive collection sourced from trusted
+            brands and suppliers.
+          </p>
+          <p className="text-gray-600 font-bold">Our Mission</p>
+
+          <p>
+            Our mission at Forever is to empower customers with choice,
+            convenience, and confidence. We're dedicated to providing a seamless
+            shopping experience that exceeds expectations, from browsing and
+            ordering to delivery and beyond.
+          </p>
         </div>
-    );
+      </div>
+      <div className="text-4xl py-4">
+        <Title text1={"WHY "} text2={"CHOOSE US"}></Title>
+      </div>
+      <NewsLetterBox></NewsLetterBox>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,31 +4,70 @@ import { Link } from "react-router-dom";
 const Login = () => {
   const [currentState, setCurrentState] = useState("Login");
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    
+  };
+
   return (
     <div>
       <form
-        action=""
+       onSubmit={handleClick}
         className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
       >
         <div className="inline-flex items-center gap-2 mb-2 mt-10">
-            <p className="prata-regular text-3xl ">{currentState}</p>
-            <hr  className="border-none h-[1.5px] w-8 bg-gray-800"/>
-
+          <p className="prata-regular text-3xl ">{currentState}</p>
+          <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
         </div>
-       {currentState === "Login" ? "": <input type="text" name="" className="rounded w-full px-3 py-2 border border-gray-800 " placeholder="Name" id="" />}
-        <input type="email" name="" className="rounded w-full px-3 py-2 border border-gray-800 " placeholder="Email" id="" />
-        <input type="password" name="" className="rounded w-full px-3 py-2 border border-gray-800 " placeholder="Password" id="" />
+        {currentState === "Login" ? (
+          ""
+        ) : (
+          <input
+            required
+            type="text"
+            name=""
+            className="rounded w-full px-3 py-2 border border-gray-800 "
+            placeholder="Name"
+            id=""
+          />
+        )}
+        <input
+          required
+          type="email"
+          name=""
+          className="rounded w-full px-3 py-2 border border-gray-800 "
+          placeholder="Email"
+          id=""
+        />
+        <input
+          required
+          type="password"
+          name=""
+          className="rounded w-full px-3 py-2 border border-gray-800 "
+          placeholder="Password"
+          id=""
+        />
         <div className="flex justify-between w-full text-sm mt-[-8px]">
-            
-            <Link className="cursor-pointer">Create account</Link>
-            <Link className="cursor-pointer">Forgot your password?</Link>
-
-
+          <Link className="cursor-pointer">Forgot your password?</Link>
+          {currentState === "Login" ? (
+            <Link
+              className="cursor-pointer"
+              onClick={() => setCurrentState("Register")}
+            >
+              Create account
+            </Link>
+          ) : (
+            <Link
+              className="cursor-pointer"
+              onClick={() => setCurrentState("Login")}
+            >
+              Login Here
+            </Link>
+          )}
         </div>
-        <Link className="bg-black text-white rounded py-3 text-sm active:bg-gray-700 px-8">Login</Link>
-
-
-
+        <Link className="bg-black text-white rounded py-3 text-sm active:bg-gray-700 px-8">
+          {currentState === "Login" ? "Login" : "Register"}
+        </Link>
       </form>
     </div>
   );
