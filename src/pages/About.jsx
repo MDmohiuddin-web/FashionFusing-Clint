162aedecc6dfbdf4440bee22517c89e91b72c4cf
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-09-24 20:27:20 +0600

    in complited

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -40,8 +40,31 @@ const About = () => {
           </p>
         </div>
       </div>
-      <div className="text-4xl py-4">
+      <div className="text-xl py-4">
         <Title text1={"WHY "} text2={"CHOOSE US"}></Title>
+
+        <div className="flex flex-col md:flex-row text-sm mb-20">
+            <div className="border px-10 md:py-16 py-8 sm:py-20 flex flex-col gap-5">
+                <p className="text-gray-600 font-bold">Quality Assurance:</p>
+                <p>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+
+            </div>
+            <div className="border px-10 md:py-16 py-8 sm:py-20 flex flex-col gap-5">
+                <p className="text-gray-600 font-bold">Convenience:</p>
+                <p>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.
+
+</p>
+
+            </div>
+            <div className="border px-10 md:py-16 py-8 sm:py-20 flex flex-col gap-5">
+                <p className="text-gray-600 font-bold">Exceptional Customer Service:</p>
+                <p>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+
+            </div>
+
+        </div>
+
+
       </div>
       <NewsLetterBox></NewsLetterBox>
     </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,18 @@
+import NewsLetterBox from "../components/NewsLetterBox";
+import Title from "../components/Title";
+
 const Contact = () => {
   return (
     <div>
-      <h3>this is contact page</h3>
+     <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"CONTACT"} text2={"US"}></Title>
+      </div>
+      <div>
+        
+      </div>
+
+
+      <NewsLetterBox></NewsLetterBox>
     </div>
   );
 };
